865c71380e42d721f6a429c9a48d20f337c2fd3f
avirup-ghosal <avirupghosal2023@gmail.com>
2025-05-18 23:08:02 +0530

    Added node.js server code

---
+++ http-server-1/index2.js
@@ -0,0 +1,34 @@
+const express=require("express");
+const app=express();
+const users=[{
+    name:"John",
+    kidneys:[{
+        healthy:false
+    }]
+}]
+app.get("/",function(req,res){
+    const johnKidneys=users[0].kidneys;
+    const numberofKidneys=johnKidneys.length;
+    let numberofHealthyKidneys=0;
+    for(let i=0;i<johnKidneys.length;i++){
+        if(johnKidneys[i].healthy){
+            numberofHealthyKidneys=numberofHealthyKidneys+1;
+        }
+    }
+    const numberofUnhealthyKidneys=numberofKidneys-numberofHealthyKidneys;
+    res.json({
+        numberofKidneys,
+        numberofHealthyKidneys,
+        numberofUnhealthyKidneys
+    })
+})
+app.post("/",function(req,res){
+    const isHealthy=req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy:isHealthy
+    })
+    res.json({
+        msg:"Done!"
+    })
+})
+app.listen(3004);
